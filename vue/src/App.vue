1504b89f830a05ceb1ff2ff394759a1243b49222
Celestial Phineas <hanghang0713@hotmail.com>
2018-05-26 10:45:05 +0800

    Init repo

---
+++ vue/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: Roboto, Raleway, 'Segoe UI', -apple-system, BlinkMacSystemFont, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', Helvetica, Arial, 'Hiragino Sans GB', 'Noto Sans CJK SC', '思源黑体', 'Source Han Sans SC', '等线', '方正等线', 'Dengxian', 'Microsoft YaHei', 'Wenquanyi Micro Hei', 'WenQuanYi Micro Hei Mono', 'WenQuanYi Zen Hei', 'WenQuanYi Zen Hei', 'Apple LiGothic Medium', 'SimHei', 'ST Heiti', 'WenQuanYi Zen Hei Sharp', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
